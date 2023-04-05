e864d4f4aeab13fff9a232898af97a19a932ab3d
William Vabrinskas <williamvabrinskas@mbp14inch2021.lan>
2023-04-05 12:11:29 -0400

    updated steps per mm

--- firmware/V2.0/Marlin-2.0.8.2.x-SKR-mini-E3-V2.0/Marlin/Configuration.h
+++ firmware/V2.0/Marlin-2.0.8.2.x-SKR-mini-E3-V2.0/Marlin/Configuration.h
@@ -812,7 +812,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80.273, 80.160, 401.312, 430}
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80.273, 80.160, 402.939, 430}
 
 /**
  * Default Max Feed Rate (mm/s)
