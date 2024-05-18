ebcbfe2beb968ba55ac69645302c4c2b60cf9966
William Vabrinskas <williamvabrinskas@mbp14inch2021.lan>
2024-05-17 21:08:15 -0400

    updated marlin

--- firmware/V2.0/Marlin-2.0.8.2.x-SKR-mini-E3-V2.0/buildroot/share/PlatformIO/scripts/marlin.py
+++ firmware/V2.0/Marlin-2.0.8.2.x-SKR-mini-E3-V2.0/buildroot/share/PlatformIO/scripts/marlin.py
@@ -17,9 +17,13 @@ def copytree(src, dst, symlinks=False, ignore=None):
             shutil.copy2(s, d)
 
 def replace_define(field, value):
+	found_define = None
 	for define in env['CPPDEFINES']:
 		if define[0] == field:
-			env['CPPDEFINES'].remove(define)
+			found_define = define
+			break
+	if found_define:
+		env['CPPDEFINES'].remove(found_define)
 	env['CPPDEFINES'].append((field, value))
 
 # Relocate the firmware to a new address, such as "0x08005000"
